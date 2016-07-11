7636a74d823600c1d26cfe5092c4ac08ed73c7f3
Alexander Alemayhu <alexander@alemayhu.com>
2016-07-11 14:27:41 +0200

    make stuff public

--- Sources/runCommand.swift
+++ Sources/runCommand.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-struct runCommand {
+public struct runCommand {
     // http://stackoverflow.com/questions/26971240/how-do-i-run-an-terminal-command-in-a-swift-script-e-g-xcodebuild
-    static func shell(args: String..., currentDirectoryPath: String) -> Int32 {
+    public static func shell(args: String..., currentDirectoryPath: String) -> Int32 {
 
         let task = Task()
         task.launchPath = "/usr/bin/env"
